1a6bf615a6acf92ce63f26e0e6521391b066e6f3
Thomas Heller <th.heller@gmail.com>
2026-04-08 10:17:39 +0200

    bump 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.thheller/shadow-http "0.1.1"
+(defproject com.thheller/shadow-http "0.1.2"
   :description "HTTP Server for shadow-cljs"
   :url "https://github.com/thheller/shadow-http"
 
